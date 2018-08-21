91dc724931360f0e094d7670cad44b37c2a962c2
lexy5251 <xixi5251@gmail.com>
2018-08-21 17:13:37 +0000

    setting

---
+++ lookup_people.js
@@ -0,0 +1,60 @@
+const pg = require('pg');
+
+const config = {
+  user: 'vagrant', //env var: PGUSER
+  database: 'db_test', //env var: PGDATABASE
+  password: 'secret', //env var: PGPASSWORD
+  host: 'localhost',
+  port: 5432 //env var: PGPORT
+};
+
+
+
+module.exports = (function() {
+
+  const db = new pg.Client(config);
+  db.connect((err) => { // Open DB connection
+    if (err) throw err;
+  })
+
+const getFamousPeople = (peopleName, callback) => {
+  let query =
+  `select first_name, birthdate
+  from famous_people
+  where famous_people.first_name, famous_people.birthdate`;
+
+  db.query(query, [peopleName], (err, result) => {
+    if(err) {
+      console.log("Something went wrong: ", err);
+      callback([]);
+    }
+    else {
+      callback(result.rows);
+    }
+  });
+}
+
+return {
+  getFamousPeople: getFamousPeople,
+  closeEverything: function() {
+    db.end();
+  }
+}
+
+})()
+
+// CREATE TABLE famous_people (
+//   id BIGSERIAL PRIMARY KEY,
+//   first_name VARCHAR(50),
+//   last_name VARCHAR(50),
+//   birthdate DATE
+// );
+
+// INSERT INTO famous_people (first_name, last_name, birthdate)
+//   VALUES ('Abraham', 'Lincoln', '1809-02-12');
+// INSERT INTO famous_people (first_name, last_name, birthdate)
+//   VALUES ('Mahatma', 'Gandhi', '1869-10-02');
+// INSERT INTO famous_people (first_name, last_name, birthdate)
+//   VALUES ('Paul', 'Rudd', '1969-04-06');
+// INSERT INTO famous_people (first_name, last_name, birthdate)
+//   VALUES ('Paul', 'Giamatti', '1967-06-06');
